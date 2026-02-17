43cff0de445d83541f036c0636cfd6fc0c1ee0d2
ShadowbytePrjkt <chesstype438@gmail.com>
2026-02-17 09:52:52 +0800

    Append necessary commits

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X6515.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X6515-user \
+    twrp_X6515-userdebug \
+    twrp_X6515-eng
